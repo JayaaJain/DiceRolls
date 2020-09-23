e8ab80cc47e0c4c86cca4aaff974956c3632df42
Jaya Jain <jayajn98@gmail.com>
2020-09-23 19:32:54 +0530

    updated

--- Desktop/BridgeLabzSessions/Review/Review2/diceRoll.sh
+++ Desktop/BridgeLabzSessions/Review/Review2/diceRoll.sh
@@ -1,4 +1,5 @@
 sum=0
+eachSum=0
 count=0
 i=0
 while [ $count -eq 0 ]
@@ -6,8 +7,10 @@ do
 var1=$((RANDOM%6+1))
 var2=$((RANDOM%6+1))
 echo "Two numbers are $var1 and $var2"
-((sum+=$var1+$var2))
-echo "sum for this roll is $sum"
+((sum+=var1+var2))
+((eachSum=var1+var2))
+echo "sum for this roll is $eachSum"
+
 ((i+=2))
 if [ $var1 -eq 6 ] && [ $var2 -eq 6 ]
 then count=2
