a46d4af2c3736c1415d74a7ee8c6e8785e34e764
Jaya Jain <jayajn98@gmail.com>
2020-09-23 19:22:36 +0530

    updated

--- Desktop/BridgeLabzSessions/Review/Review2/diceRoll.sh
+++ Desktop/BridgeLabzSessions/Review/Review2/diceRoll.sh
@@ -1,11 +1,13 @@
 sum=0
 count=0
+i=0
 while [ $count -eq 0 ]
 do
 var1=$((RANDOM%6+1))
 var2=$((RANDOM%6+1))
 echo "Two numbers are $var1 and $var2"
 ((sum+=$var1+$var2))
+((i+=2))
 if [ $var1 -eq 6 ] && [ $var2 -eq 6 ]
 then count=2
 #echo "Sum till now is $sum"
@@ -14,3 +16,4 @@ echo "Sum till now is $sum"
 break;
 fi
 done
+echo "Total number of rolls : $i"
